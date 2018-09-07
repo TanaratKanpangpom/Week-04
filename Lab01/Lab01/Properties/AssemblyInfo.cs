703b4621bb947fefedfdc81e495c493339221ba1
USER NAME <55030402@kmitl.ac.th>
2018-09-07 15:20:38 +0700

    W04P05

--- Lab01/Lab01/Properties/AssemblyInfo.cs
+++ Lab01/Lab01/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyTitle("Lab01")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyProduct("Lab01")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("70430a42-c32d-4a52-b113-5bc58e6b2aec")]
+[assembly: Guid("2730e475-5314-4516-ab77-3a6ca8ab6185")]
 
 // Version information for an assembly consists of the following four values:
 //
